e05b72960cc09895ea533eaa5097e743c3bc1fca
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-21 16:27:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
   def self.alphabetical
   self.all.sort_by {|song| song.name}
   end
-  def self.new_from_filename
+  def self.new_from_filename(filename)
     artist_name, song_name = filename.split (" - ")
     song = self.new_by_name(song_name.chomp (".mp3"))
     song.artist_name = artist_name
